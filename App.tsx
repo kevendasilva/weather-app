e0bb29ce5832e60c4667ea3ca8c00d43d33ab9b3
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-05-23 18:48:30 -0300

    creating container component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+import { Container } from './src/components/Container';
+import { Location } from './src/components/Location'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Container.tsx
@@ -0,0 +1,23 @@
+import { FunctionComponent } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+type ContainerProps = {
+  children: React.ReactNode
+}
+
+export const Container: FunctionComponent<ContainerProps> = ({ children }) => {
+  return (
+    <View style={styles.container}>
+      { children }
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F9F93B',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

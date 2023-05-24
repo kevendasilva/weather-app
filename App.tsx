9fe8cb6a5931f9a8491532e9445ba1e389c5b82a
Keven da Silva Gonçalves <kevensilvag@alu.ufc.br>
2023-05-23 21:57:58 -0300

    creating daily summary component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { Container } from './src/components/Container';
+import { DailySummary } from './src/components/DailySummary';
 import { DateTime } from './src/components/DateTime';
 import { Location } from './src/components/Location';
 import { Temperature } from './src/components/Temperature';
@@ -9,8 +10,12 @@ export default function App() {
     <Container>
       <Location city="Fortaleza" state="CE" />
       <DateTime datetime="terça-feira, 20 de maio" />
-      <Weather temperature="" wind="" humidity=""/>
+      <Weather temperature="" wind="" humidity="" />
       <Temperature temperatureInDegrees="31º" />
+      <DailySummary
+        content="Agora, sensação térmica de +34º.
+Possíveis pancadas de chuva ao final do dia."
+      />
     </Container>
   );
 }
---
+++ src/components/DailySummary.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+type DailySummaryProps = {
+  content: string
+}
+
+export const DailySummary: FunctionComponent<DailySummaryProps> = ({ content }) => {
+  return (
+    <View style={styles.dailySummaryContainer}>
+      <Text style={styles.dailySummary}>Resumo diário</Text>
+      <Text style={styles.dailySummary}>{content}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  dailySummary: {
+    fontFamily: 'Roboto',
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#000000',
+  },
+  dailySummaryContainer: {
+    gap: 16
+  }
+});
